7f7d430a3f0980a07d7dccf859f0b1efa7f68a03
acies-pritika-karthikeyan <pritika.karthikeyan@aciesglobal.com>
2024-02-28 15:01:43 +0530

    changes2

--- AddDetails.py
+++ AddDetails.py
@@ -1,6 +1,14 @@
 from ProjectDetails import ProjectDetails
 from Employee import Employee
-class AddDetails:
+class AddDetails(empId,empName, age, salary, projectName, team):
+    def __init__(self, empId,empName, age, salary, projectName, team):
+        self.empId = empId
+        self.empName = empName
+        self.age = age
+        self.salary = salary
+        self.projectName = projectName
+        self.team = team
+
     def addProd(name, stage, team):
         project_details = ProjectDetails(name, stage,team)
         return project_details
--- main.py
+++ main.py
@@ -9,13 +9,8 @@ from Technology import Technology
 from AddDetails import AddDetails
 
 def main():
-    addDetails = AddDetails()
-    emp = addDetails.addEmp(2, "Aryan", 21, 12000)
-    project_details = addDetails.addProd("DS", "Clean","Analytics")
-    # ds = Analytics(emp, project_details)
-
-    emp = addDetails.addEmp(3, "Subhash", 24, 12500)
-    project_details = addDetails.addProd("HR", "recuriment","HR")
+    addDetails = AddDetails(2, "Aryan",21,1200,'DS',"Clean","Analytics")
+    addDetails = AddDetails(3, "Subhash",24,12500,'HR',"Clean","Analytics")
     # hr = HR(emp, project_details)
 if __name__ == "__main__":
     main()
